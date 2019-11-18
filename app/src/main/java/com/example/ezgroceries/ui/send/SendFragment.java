915b2cf926da4ecbeb959a4a93b8ea3aa211af9b
dmgcsilva <43959937+dmgcsilva@users.noreply.github.com>
2019-11-18 18:33:41 +0000

    update

--- app/src/main/java/com/example/ezgroceries/ui/send/SendFragment.java
+++ app/src/main/java/com/example/ezgroceries/ui/send/SendFragment.java
@@ -1,9 +1,11 @@
 package com.example.ezgroceries.ui.send;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,6 +14,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.ezgroceries.HomeScreen;
+
+import com.example.ezgroceries.MainMenu;
 import com.example.ezgroceries.R;
 
 public class SendFragment extends Fragment {
@@ -28,6 +33,24 @@ public class SendFragment extends Fragment {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
+            }
+        });
+
+        Button yesButton = (Button) root.findViewById(R.id.sair);
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HomeScreen.class);
+                startActivity(intent);
+            }
+        });
+
+        Button noButton = (Button) root.findViewById(R.id.naoSair);
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainMenu.class);
+                startActivity(intent);
             }
         });
         return root;
