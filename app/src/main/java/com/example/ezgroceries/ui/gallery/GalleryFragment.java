48ffb0413bdde7442c41deeb815fb782f52abb34
abgrilo4 <57844157+abgrilo4@users.noreply.github.com>
2019-11-23 19:05:51 +0000

    alterações da bia

--- app/src/main/java/com/example/ezgroceries/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/ezgroceries/ui/gallery/GalleryFragment.java
@@ -1,9 +1,13 @@
 package com.example.ezgroceries.ui.gallery;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,14 +18,20 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.ezgroceries.R;
 
+import java.util.ArrayList;
+
 public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
+    ArrayList<String> listaFavoritos;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
+
+        listaFavoritos = new ArrayList<>();
+        listaFavoritos.add("Fiambre");
+        listaFavoritos.add("Queijo");
+        galleryViewModel = ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
         galleryViewModel.getText().observe(this, new Observer<String>() {
@@ -30,6 +40,31 @@ public class GalleryFragment extends Fragment {
                 textView.setText(s);
             }
         });
-        return root;
+
+
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        final ListView listView = view.findViewById(R.id.ListaFavoritos);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_multiple_choice,
+                listaFavoritos);
+        listView.setAdapter(adapter);
+
+        Button deleteButton = (Button) view.findViewById(R.id.DeleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SparseBooleanArray positionChecker = listView.getCheckedItemPositions();
+                int count = listView.getCount();
+                for (int item = count-1; item>=0; item--){
+                    if(positionChecker.get(item)){
+                        adapter.remove(listaFavoritos.get(item));
+                    }
+                }
+            }
+        });
+
+
+        return view;
     }
 }
